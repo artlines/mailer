435dfb72aad2ed618d3c7b86fc51a78dbc6b2652
alf1kknag <e.nachuychenko@nag.ru>
2018-07-09 15:17:27 +0500

    add support raw request with JSON string

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -36,6 +36,9 @@ class MailerController extends AbstractController
      */
     private $logger;
 
+    /** @var array */
+    private $requestData = [];
+
     /**
      * MailerController constructor.
      * @param Logger $logger
@@ -57,13 +60,15 @@ class MailerController extends AbstractController
      */
     public function send(Request $request, EmailManager $emailManager, AuthManager $auth)
     {
+        $this->_getRequestParams($request);
+
         try {
-            $this->_checkRequireParamsExists($request);
+            $this->_checkRequireParamsExists();
 
-            $this->_getClient($request->request->get('client_alias'));
-            $this->_getTemplate($request->request->get('template_alias'));
+            $this->_getClient($this->requestData['client_alias']);
+            $this->_getTemplate($this->requestData['template_alias']);
 
-            $auth->checkAccessByClient($request, $this->client);
+            $auth->checkAccessByClient($request, $this->client, $this->requestData);
 
             if (!$this->template->isActive()) {
                 throw new \Exception("Template with alias '{$this->template->getAlias()}' is not active.");
@@ -75,19 +80,14 @@ class MailerController extends AbstractController
                 );
             }
 
-            $_sender = $request->request->get('sender');
-            if (null === $_sender) {
-                $_sender = $this->client->getSender();
-            }
-
-            $bodyData = $emailManager->generateBodyFromTemplate($this->template, $request->request->get('params'), EmailManager::CONTENT_HTML);
+            $bodyData = $emailManager->generateBodyFromTemplate($this->template, $this->requestData['params'], EmailManager::CONTENT_HTML);
             $response = $emailManager->send(
-                $request->request->get('subject'),
+                $this->requestData['subject'],
                 $bodyData,
-                $_sender,
-                $request->request->get('send_to'),
-                $request->request->get('send_cc') ?? [],
-                $request->request->get('send_bcc') ?? []
+                $this->requestData['sender'] ?? $this->client->getSender(),
+                $this->requestData['send_to'],
+                $this->requestData['send_cc'] ?? [],
+                $this->requestData['send_bcc'] ?? []
             );
 
             $id = $this->logger->logMail($response['sm'], $request, $response['status']);
@@ -135,13 +135,27 @@ class MailerController extends AbstractController
     }
 
     /**
-     * Check require parameters
+     * Достает параметры запроса в ассоциативный массив
+     * Парсит как обычный запрос, так и запрос с JSON строкой
      *
-     * @param $request
+     * @author Evgeny Nachuychenko e.nachuychenko@nag.ru
+     * @param Request $request
+     */
+    private function _getRequestParams(Request $request)
+    {
+        if ($request->getContentType() === 'json') {
+            $this->requestData = json_decode($request->getContent(), true);
+        } else {
+            $this->requestData = $request->request->all();
+        }
+    }
+
+    /**
+     * Check require parameters
      *
      * @throw BadRequestHttpException If any param doesn't exist
      */
-    private function _checkRequireParamsExists($request)
+    private function _checkRequireParamsExists()
     {
         $_requireParams = [
             'client_alias',
@@ -155,7 +169,7 @@ class MailerController extends AbstractController
 
         foreach ($_requireParams as $_param)
         {
-            if (null === $request->request->get($_param)) {
+            if (null === $this->requestData[$_param]) {
                 throw new BadRequestHttpException("Missing '$_param' parameter.");
             }
         }
